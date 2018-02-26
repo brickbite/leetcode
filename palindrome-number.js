718b914af2638424f2d5f80d75b222b556e492f0
Jeffrey Lee <jeffreylee0116@gmail.com>
2018-02-25 16:48:31 -0800

    Create palindrome-number.js

---
+++ palindrome-number.js
@@ -0,0 +1,40 @@
+/*
+Determine whether an integer is a palindrome. Do this without extra space.
+
+Some hints:
+Could negative integers be palindromes? (ie, -1)
+
+If you are thinking of converting the integer to string, note the restriction of using extra space.
+
+You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", you know that the reversed integer might overflow. How would you handle such case?
+
+There is a more generic way of solving this problem.
+ */
+
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0) {
+        return false;
+    }
+    
+    let digits = 1;
+    while (Math.floor(x / digits) >= 10) {
+        digits *= 10;
+    }
+    
+    while (x) {
+        if (x % 10 !== (x - (x % (digits)))/(digits)) {
+            return false
+        }
+        x = Math.floor(x / 10);
+        digits /= 10;
+        x = x % digits;
+        digits /= 10;
+    }
+    
+    return true;
+};
