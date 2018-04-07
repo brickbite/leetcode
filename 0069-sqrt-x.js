27698f975c3e72dc48ca6b769c69794ee1c085a4
Jeffrey Lee <jeffreylee0116@gmail.com>
2018-04-07 00:44:07 -0700

    Create 0069-sqrt-x.js

---
+++ 0069-sqrt-x.js
@@ -0,0 +1,39 @@
+/*
+69. Sqrt(x)
+
+Implement int sqrt(int x).
+
+Compute and return the square root of x.
+
+x is guaranteed to be a non-negative integer.
+
+
+Example 1:
+
+Input: 4
+Output: 2
+
+Example 2:
+
+Input: 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we want to return an integer, the decimal part will be truncated.
+*/
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    let current = 1
+    
+    while (current * current < x) {
+        current++;
+    }
+    
+    if (current * current === x ) {
+        return current;
+    } else if (current * current > x) {
+        return current - 1;
+    }
+};
