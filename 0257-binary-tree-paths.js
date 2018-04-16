5d46b876e2b850c4265917b22ee9e26342dea0c8
Jeffrey Lee <jeffreylee0116@gmail.com>
2018-04-16 03:38:39 -0700

    Create 0257-binary-tree-paths.js

---
+++ 0257-binary-tree-paths.js
@@ -0,0 +1,42 @@
+/*
+257. Binary Tree Paths
+
+Given a binary tree, return all root-to-leaf paths.
+
+For example, given the following binary tree:
+
+   1
+ /   \
+2     3
+ \
+  5
+All root-to-leaf paths are:
+
+["1->2->5", "1->3"]
+Credits:
+Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root, str = ``) {
+    if (!root) {
+        return [];
+    }
+    
+    if (!root.left && !root.right) {
+        return [str + `${root.val}`];
+    }
+    
+    return [].concat(binaryTreePaths(root.left, str + `${root.val}->`)).concat(binaryTreePaths(root.right, str + `${root.val}->`));
+    
+};
