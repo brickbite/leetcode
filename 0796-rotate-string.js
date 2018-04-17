87768003a6f6b069329f69694634847e292df009
Jeffrey Lee <jeffreylee0116@gmail.com>
2018-04-17 12:15:47 -0700

    Create 0796-rotate-string.js

---
+++ 0796-rotate-string.js
@@ -0,0 +1,44 @@
+/*
+796. Rotate String
+
+We are given two strings, A and B.
+
+A shift on A consists of taking string A and moving the leftmost character to the rightmost position. For example, if A = 'abcde', then it will be 'bcdea' after one shift on A. Return True if and only if A can become B after some number of shifts on A.
+
+Example 1:
+Input: A = 'abcde', B = 'cdeab'
+Output: true
+
+Example 2:
+Input: A = 'abcde', B = 'abced'
+Output: false
+Note:
+
+A and B will have length at most 100.
+*/
+
+/**
+ * @param {string} A
+ * @param {string} B
+ * @return {boolean}
+ */
+var rotateString = function(A, B) {
+        
+    if (A.length !== B.length) {
+        return false;
+    }
+    
+    if (A.length === 0 || B.length === 0) {
+        return true;
+    }
+    
+    for (let i = 0; i < A.length; i++) {
+        if (A === B) {
+            return true;
+        }
+        
+        A = A.slice(1, A.length) + A.charAt(0);
+    }
+    
+    return false;
+};
