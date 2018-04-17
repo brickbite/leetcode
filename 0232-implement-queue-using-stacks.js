7468b00cd9475fdc76fcc2818340585a27be55e1
Jeffrey Lee <jeffreylee0116@gmail.com>
2018-04-17 02:55:55 -0700

    Create 0232-implement-queue-using-stacks.js

---
+++ 0232-implement-queue-using-stacks.js
@@ -0,0 +1,75 @@
+/*
+232. Implement Queue using Stacks
+
+Implement the following operations of a queue using stacks.
+
+push(x) -- Push element x to the back of queue.
+pop() -- Removes the element from in front of queue.
+peek() -- Get the front element.
+empty() -- Return whether the queue is empty.
+Notes:
+You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+*/
+
+/**
+ * Initialize your data structure here.
+ */
+var MyQueue = function() {
+    this.s1 = [];
+    this.s2 = [];
+};
+
+/**
+ * Push element x to the back of queue. 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.s1.push(x);
+    return x;
+};
+
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    while (this.s1.length > 1) {
+        this.s2.push(this.s1.pop());
+    }
+    
+    let dequeued = this.s1.pop();
+    
+    while (this.s2.length > 0) {
+        this.s1.push(this.s2.pop());
+    }
+    
+    return dequeued;
+};
+
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    return this.s1[0];
+};
+
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.s1.length === 0;
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = Object.create(MyQueue).createNew()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
